5418fc97a4babe14f02245f68d1d4b58ec55d062
Sharda <53011896+shardapatil@users.noreply.github.com>
2023-07-03 20:40:34 +0530

    Add files via upload

---
+++ Employee.cs
@@ -0,0 +1,215 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace MyWebAPI
+{
+    public class Employee
+    {
+        public int EmpNo { get; set; }
+        public string Name { get; set; }
+        public decimal Basic { get; set; }
+        public int DeptNo { get; set; }
+
+        public static List<Employee> GetAllEmployees()
+        {
+            List<Employee> empLst = new List<Employee>();
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSqlLocalDb;Initial Catalog=KTjune23;Integrated Security=true";
+            cn.Open();
+
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "select * from Employees";
+
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                while (dr.Read())
+                {
+                    Employee e = new Employee();
+                    e.EmpNo = dr.GetInt32("EmpNo");
+                    e.Name = dr.GetString("Name");
+                    e.Basic = dr.GetDecimal("Basic");
+                    e.DeptNo = dr.GetInt32("DeptNo");
+
+                    empLst.Add(e);
+
+                }
+                dr.Close();
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+
+            return empLst;
+        }
+
+        public static Employee GetSingleEmployee(int EmpNo)
+        {
+            Employee? emp = new Employee();
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSqlLocalDb;Initial Catalog=KTjune23;Integrated Security=true";
+            cn.Open();
+
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.Text;
+                cmd.CommandText = "select * from Employees where empno=@EmpNo";
+
+                cmd.Parameters.Add("@EmpNo", SqlDbType.Int).Value = EmpNo;
+
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                if (dr.Read())
+                {
+                    emp.EmpNo = dr.GetInt32("EmpNo");
+                    emp.Name = dr.GetString("Name");
+                    emp.Basic = dr.GetDecimal("Basic");
+                    emp.DeptNo = dr.GetInt32("DeptNo");
+                }
+                else
+                {
+                    emp = null;
+                    // Console.WriteLine("Employee not found!");
+                }
+                dr.Close();
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+
+            return emp;
+        }
+
+        public static int Update(Employee obj)
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSqlLocalDb;Initial Catalog=KTjune23;Integrated Security=true";
+            cn.Open();
+
+            try
+            {
+                //SqlCommand cmd = cn.CreateCommand();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "UpdateEmmployee";
+
+                cmd.Parameters.AddWithValue("@EmpNo", obj.EmpNo);
+                cmd.Parameters.AddWithValue("@Name", obj.Name);
+                cmd.Parameters.AddWithValue("@Basic", obj.Basic);
+                cmd.Parameters.AddWithValue("@DeptNo", obj.DeptNo);
+
+                int Status = cmd.ExecuteNonQuery();
+
+                if (Status > 0)
+                {
+                    return 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+            return 0;
+        }
+
+
+        //delete using sp
+        public static int Delete(int empno)
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSqlLocalDb;Initial Catalog=KTjune23;Integrated Security=true";
+            cn.Open();
+
+            try
+            {
+                //SqlCommand cmd = cn.CreateCommand();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "DeleteEmployee";
+
+                cmd.Parameters.AddWithValue("@empno", empno);
+
+                int Status = cmd.ExecuteNonQuery();
+
+
+                if (Status > 0)
+                {
+                    return 1;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+            return 0;
+        }
+
+        public static int InsertEmployee(Employee obj)
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\MSSqlLocalDb;Initial Catalog=KTjune23;Integrated Security=true";
+            cn.Open();
+
+            try
+            {
+                //SqlCommand cmd = cn.CreateCommand();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = cn;
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "InsertEmployee";
+
+                cmd.Parameters.AddWithValue("@EmpNo", obj.EmpNo);
+                cmd.Parameters.AddWithValue("@Name", obj.Name);
+                cmd.Parameters.AddWithValue("@Basic", obj.Basic);
+                cmd.Parameters.AddWithValue("@DeptNo", obj.DeptNo);
+
+                int cnt = cmd.ExecuteNonQuery();
+
+                if (cnt > 0)
+                {
+                    return 1;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                cn.Close();
+            }
+            return 0;
+        }
+    }
+
+}
